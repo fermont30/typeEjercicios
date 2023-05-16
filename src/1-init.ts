b7dea9a8439cb096903edddbe1d8840d926043a5
fermont30 <lfq.montoya@yavirac.edu.ec>
2023-05-15 20:31:07 -0500

    ejercicios type

---
+++ src/1-init.ts
@@ -0,0 +1,12 @@
+//init
+
+console.log("Hola Mundo");
+
+var numero1: number = 10;
+var numero2: number = 12;
+
+if(numero1 === numero2){
+  console.log("Son iguales");
+}else{
+  console.log("No son iguales");
+}
