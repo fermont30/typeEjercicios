ba9a66a4406f586480f4c2b66882f47ffa2c16b9
fermont30 <lfq.montoya@yavirac.edu.ec>
2023-05-18 19:55:32 -0500

    ultimo cambio

--- src/5-boolean.ts
+++ src/5-boolean.ts
@@ -1,20 +1,19 @@
 //booleans
 
 (()=>{
-    let isEnable = true;
+let isEnable = true;
 
-    // isEnable = 'as';
+isEnable = false;
+let isNew: boolean = false;
+console.log('isNew : ', isNew);
+isNew = true;
+console.log('isNew : ', isNew);
 
-    // isEnable = 1212
+const random = Math.random();
+console.log('random : ', random);
+isNew = random >- 0.5 ? true : false;
+console.log('isNew', isNew);
 
-    isEnable = false;
+const myBoolean: boolean = true;
+})();
 
-    let isNew: boolean = false;
-    console.log('isNew: ', isNew);
-
-    isNew = true;
-    console.log('isNew : ', isNew);
-
-    const random = Math.random();
-    console.log('random : ', random);
-})
